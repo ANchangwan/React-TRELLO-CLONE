ab67aa7dc7c4428e8bbe46b42a4e28ec94adc19e
An chang wan <ssdii66@gmail.com>
2022-09-16 00:18:28 +0900

    cross Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ function App() {
   const onDragEnd = (info: DropResult) => {
     console.log(info);
     const { destination, draggableId, source } = info;
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       /// 같은 보드 안에서만 작동
       setToDos((allBoards) => {
@@ -46,6 +47,20 @@ function App() {
         return {
           ...allBoards,
           [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination.droppableId !== source.droppableId) {
+      // cross board movement
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
         };
       });
     }
